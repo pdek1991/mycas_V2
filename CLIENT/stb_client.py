301d911591b5e70701b04808b70182296ecda3b1
pdek1991 <94885346+pdek1991@users.noreply.github.com>
2025-03-21 19:01:34 +0530

    Update stb_client.py

--- CLIENT/stb_client.py
+++ CLIENT/stb_client.py
@@ -9,16 +9,18 @@ import pyarmor
 import os
 # Define the multicast group and port
 
-multicast_group = os.getenv("MULTICAST", "224.1.1.1")  
-port = int(os.getenv("PORT", 5000))
+#multicast_group = os.getenv("MULTICAST", "224.1.1.1").strip()
+#port = int(os.getenv("PORT", 5000))
 
+multicast_group = "0.0.0.0"
+port = 5050
 key = 'qwertyuioplkjhgd'
 start_time = time.time()
 total_bytes = 0
 
 
 logging.basicConfig(
-    level=logging.INFO,  # Set log level
+    level=logging.DEBUG,  # Set log level
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)]
 )
@@ -30,12 +32,9 @@ logger.info(f"Listening on {multicast_group} and Port {port}")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to a specific interface and port
-sock.bind(('', port))
-logger.info("Bind succesfull")
-# Join the multicast group
-group = socket.inet_aton(multicast_group)
-mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+sock.bind((multicast_group, port))
+#logger.info("Bind succesfull")
+
 
 # Create an AES cipher object with the provided key and CTR mode
 def decrypt_string(key, encrypted_data):
@@ -56,6 +55,7 @@ def decrypt_string(key, encrypted_data):
     # Remove the padding from the plaintext
     padding_length = ord(padded_plaintext[-1])
     plaintext = padded_plaintext[:-padding_length]
+    #logger.info(f"Data received: {plaintext}")
     return plaintext
 
 try:
@@ -63,19 +63,22 @@ try:
         # Receive and process data from the multicast stream
         data, address = sock.recvfrom(1024)
         plaintext = decrypt_string(key, data)
-        #print(f"Received data: {plaintext}")
-        total_bytes += len(data)
-        
-        elapsed_time = time.time() - start_time
-        if elapsed_time >= 30:
-            # Calculate bytes per second
-            bytes_per_sec = total_bytes / elapsed_time
-            # Print the result
-            logger.info(f"BW in Kb:, {bytes_per_sec}/1024")
-            #print("BW in Kb:", bytes_per_sec/1024)
-            # Reset counters and start time
-            total_bytes = 0
-            start_time = time.time()
+        if plaintext.startswith("982334"):
+            logger.info(f"Received encryoted data: {data} from {address}")
+            logger.info(f"Received data: {plaintext}")
+            total_bytes += len(data)
+            logger.info(f"Total Bytes:, {total_bytes}")
+            elapsed_time = time.time() - start_time
+            logger.info(f"Elapsed Time:, {elapsed_time}")
+            if elapsed_time >= 30:
+                # Calculate bytes per second
+                bytes_per_sec = total_bytes / elapsed_time
+                # Print the result
+                logger.info(f"BW in Kb:, {bytes_per_sec}/1024")
+                #print("BW in Kb:", bytes_per_sec/1024)
+                # Reset counters and start time
+                total_bytes = 0
+                start_time = time.time()
             
 except KeyboardInterrupt:
     # Handle Ctrl+C interruption
